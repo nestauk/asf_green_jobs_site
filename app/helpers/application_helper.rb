d3f216faaa0f0427a8d3998067437aba89b48a29
Suraj Vadgama <suninthesky@users.noreply.github.com>
2024-02-02 14:28:19 +0000

    Update card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,12 @@
 module ApplicationHelper
-  def greenness_bg(rating)
+  def third_to_words(rating)
     case rating
     when 'high'
-      ' bg-emerald-700 text-white'
-    when 'mid-high'
-      ' bg-lime-700 text-white'
-    when 'low-mid'
-      ' bg-cyan-700 text-white'
+      'highest'
+    when 'mid'
+      'middle'
     when 'low'
-      ' bg-amber-700 text-white'
+      'lowest'
     end
   end
 end
